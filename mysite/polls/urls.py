7ceac270605ec669ba0161720ccbea82973d110a
Prateek Mukherjee <muprat172@gmail.com>
2021-03-29 22:06:42 +0530

    First commit

---
+++ mysite/polls/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from .models import Question
+
+from . import views
+
+app_name='polls'
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+urlpatterns = [
+    # ex: /polls/
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /polls/5/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # ex: /polls/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
